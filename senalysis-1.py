8c33f4213f8b7e99cc22119676f63318d9e56773
Rajib Biswas <rajib@improlabs.com>
2017-07-23 21:16:16 +0600

    initial commit

---
+++ senalysis-1.py
@@ -0,0 +1,24 @@
+#!/usr/local/bin/python
+
+from pyavrophonetic import avro
+from textblob import TextBlob
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+
+import sys
+
+b = raw_input("Enter Banglish :")
+
+textBlobTrans = TextBlob(avro.parse(b))
+
+c = str(textBlobTrans.translate(to='en'))
+print(c)
+
+analyze = TextBlob(c)
+print('By TextBlob Sentiment analysis')
+print(analyze.sentiment)
+
+print('By vaderSentiment analyzer')
+analyzer = SentimentIntensityAnalyzer()
+vs = analyzer.polarity_scores(c)
+
+print(str(vs))
